2338dc5c607ac91446226a8cd6ab78d8f74f13f0
PatrickBeh <93169933+PatrickBeh@users.noreply.github.com>
2022-05-09 23:37:26 +1000

    Introduction to Laravel

--- database/migrations/2022_05_09_130659_create_posts_table.php
+++ database/migrations/2022_05_09_130659_create_posts_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->boolean('isAdmin');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('subtitle');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
